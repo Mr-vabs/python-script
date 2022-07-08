8dfec09e1fa4387a5bc86d6195fa5089e443324f
Vaibhav <33443357+Mr-vabs@users.noreply.github.com>
2022-07-08 17:34:02 +0530

    Update admit.py
    Enhancement of admit.py

--- admit.py
+++ admit.py
@@ -18,8 +18,39 @@ def downloadasfile(url, filename):
         print(url)
 '''
 
-ccode = input("\n\nEnter college code :: ")
-enum = input("\n\nEnter enrollment number:: ")
-url = "https://bteup.ac.in/AdmitCardVerificationCard/AdmitCard/"+ccode+"/"+enum+".pdf"
-filename = enum+".pdf"
-download(url,filename)
+
+
+choice = 1
+print("Not supported in Android for now\n")
+while choice :
+    print("Download single or multiple results?")
+    print("1. Single Result\n2. Multiple Results")
+    choice = int(input("3. Exit\n"))
+    
+    if choice == 1 :
+        ccode = input("\n\nEnter college code :: ")
+        enum = input("\n\nEnter enrollment number:: ")
+        url = "https://bteup.ac.in/AdmitCardVerificationCard/AdmitCard/"+ccode+"/"+enum+".pdf"
+        filename = enum+".pdf"
+        download(url,filename)
+    
+    if choice == 2 :
+        ccode = input("\n\nEnter college code :: ")
+        print("Enter range :: ")
+        lowenum = input("From this :: ")
+        upenum = input("To this :: ")
+        low = int(lowenum[1::])
+        up = int(upenum[1::])
+        up = up + 1
+        
+        for i in range(low, up) :
+            enum = upenum[:1:]
+            i = f'{i}'
+            enum = enum + i
+            url = "https://bteup.ac.in/AdmitCardVerificationCard/AdmitCard/"+ccode+"/"+enum+".pdf"
+            filename = enum+".pdf"
+            download(url,filename)
+    
+    else :
+        choice = 0
+        print("bye bye...:D")
